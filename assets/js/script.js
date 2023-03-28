72e6768a37cf2da54b40260ba14bca55b72f0cb7
Sanjid Dad <sanjiddad9@gmail.com>
2023-03-28 15:39:40 +0600

    Navbar Fixed script Done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ window.addEventListener("load", function () {
     loader.style.display = "none";
 })
 // ======================== PRELOADER ========================
+// ======================== NAVBAR FIXED ========================
+let nav = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+    if (this.scrollY > 250) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
+    }
+})
+// ======================== NAVBAR FIXED ========================
