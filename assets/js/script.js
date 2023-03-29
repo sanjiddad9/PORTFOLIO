0dd3e2b00dc1f51ba81ad478e7fbf9c218f451f8
Sanjid Dad <sanjiddad9@gmail.com>
2023-03-29 14:30:28 +0600

    Portfolio Filter add

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,25 @@ window.addEventListener("scroll", function () {
     }
 })
 // ======================== NAVBAR FIXED ========================
+// ======================== PORTFOLIO FILTER ========================
+let filterNumber = document.getElementById("filter").children;
+let itemNumber = document.getElementById("projects").children;
+for (let i = 0; i < filterNumber.length; i++) {
+    filterNumber[i].addEventListener("click", function () {
+        for (let j = 0; j < filterNumber.length; j++) {
+            filterNumber[j].classList.remove("active");
+        }
+        this.classList.add("active");
+        let target = this.getAttribute("data-target");
+        for (let k = 0; k < itemNumber.length; k++) {
+            itemNumber[k].style.display = "none";
+            if (target == itemNumber[k].getAttribute("data-id")) {
+                itemNumber[k].style.display = "block";
+            }
+            if (target == "all") {
+                itemNumber[k].style.display = "block";
+            }
+        }
+    })
+}
+// ======================== PORTFOLIO FILTER ========================
